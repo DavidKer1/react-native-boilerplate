e58689c79be86b28747b0c6bac01ddcaa4b24e0f
Victor Sanchez <victor.sanchez@sonrisasmexicanas.com>
2022-06-14 18:37:18 -0500

    feat: Alias configure

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,31 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
-  };
+const tsconfig = require("./tsconfig.json");
+let rawAlias = tsconfig.compilerOptions.paths;
+let alias = {};
+
+for (let x in rawAlias) {
+	alias[x.replace("/*", "")] = rawAlias[x].map((p) => p.replace("/*", ""));
+}
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+		plugins: [
+			"react-native-reanimated/plugin",
+			[
+				"module-resolver",
+				{
+					root: ["./"],
+					extensions: [
+						".ios.js",
+						".android.js",
+						".js",
+						".ts",
+						".tsx",
+						".json",
+					],
+					alias,
+				},
+			],
+		],
+	};
 };
